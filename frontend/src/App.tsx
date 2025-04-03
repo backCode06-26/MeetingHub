d583651e6687df8023e4a099eaf17579bb13b6bf
hojin <hojin1005@gmail.com>
2025-04-03 10:16:08 +0900

    update Vite, list, button, meun add

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,87 @@
-import React from 'react';
-import './App.css';
-import Nav from './component/Nav';
-import Main from './component/Main';
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import {
+    Menubar,
+    MenubarMenu,
+    MenubarTrigger,
+} from "@/components/ui/menubar"
+import { Button } from "@/components/ui/button"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { Separator } from "@/components/ui/separator"
+
+import "./App.css";
+
+const tags = Array.from({ length: 50 }).map(
+    (_, i, a) => `v1.2.0-beta.${a.length - i}`
+)
+
 
 function App() {
-  return (
-    <div className="App">
-      <Nav isLoggedIn={false}></Nav>
-      <Main></Main>
-    </div>
-  );
+
+    return (
+        <>
+            <Menubar>
+                <MenubarMenu>
+                    <MenubarTrigger>로그인</MenubarTrigger>
+                    <MenubarTrigger>회원가입</MenubarTrigger>
+                    <MenubarTrigger>회원정보</MenubarTrigger>
+                    {/* <MenubarContent>
+            <MenubarItem>
+              New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+            </MenubarItem>
+            <MenubarItem>New Window</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Share</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Print</MenubarItem>
+          </MenubarContent> */}
+                </MenubarMenu>
+            </Menubar>
+
+            {/* spring에서 예약 정보 받음 */}
+            <div className="flex justify-center items-center">
+                <ScrollArea className="h-72 w-48 rounded-md border mx-auto">
+                    <div className="p-4">
+                        <h4 className="mb-4 text-sm font-medium leading-none">Tags</h4>
+                        {tags.map((tag) => (
+                            <div key={tag}>
+                                <div className="text-sm">{tag}</div>
+                                <Separator className="my-2" />
+                            </div>
+                        ))}
+                    </div>
+                </ScrollArea>
+            </div>
+
+            <AlertDialog>
+                <AlertDialogTrigger asChild>
+                    <Button variant="outline">회의실 예약</Button>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                    <AlertDialogHeader>
+                        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                        <AlertDialogDescription>
+                            This action cannot be undone. This will permanently delete your
+                            account and remove your data from our servers.
+                        </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        <AlertDialogAction>Continue</AlertDialogAction>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
+        </>
+    )
 }
 
-export default App;
+export default App
